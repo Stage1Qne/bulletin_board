c37f689182a01c1227801c2a008ac7b380142693
Alex Deme <stage1qne@gmail.com>
2015-11-04 16:07:24 +0300

    add PictureUploader and picture to ads

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104112501) do
+ActiveRecord::Schema.define(version: 20151104125709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20151104112501) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "picture"
   end
 
   add_index "ads", ["user_id"], name: "index_ads_on_user_id", using: :btree
