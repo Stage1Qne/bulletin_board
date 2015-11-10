5bbdcbff6db0f261799a5ddcd27d6f78b2676adb
Alex Deme <stage1qne@gmail.com>
2015-11-10 12:03:24 +0300

    flash_message helper

--- app/helpers/admin_panel_helper.rb
+++ app/helpers/admin_panel_helper.rb
@@ -6,4 +6,12 @@ module AdminPanelHelper
   def required_field_class(action)
     %w(new create).include?(action) ? 'required' : ''
   end
+
+  def flash_message(class_name, header_text, messages)
+    render partial: 'admin_panel/partials/flash_message', locals: {
+      class_name: class_name,
+      header_text: header_text,
+      messages: messages
+    }
+  end
 end
