4fe9887ec4c23525708812aafa4384aa6a2d8c54
Alex Deme <stage1qne@gmail.com>
2015-11-17 14:47:52 +0300

    admin_panel base controller load_and_authorize_resource

--- app/controllers/admin_panel/base_controller.rb
+++ app/controllers/admin_panel/base_controller.rb
@@ -1,7 +1,12 @@
 module AdminPanel
   class BaseController < ActionController::Base
     layout 'admin_panel'
+    load_and_authorize_resource
 
     protect_from_forgery with: :exception
+
+    rescue_from CanCan::AccessDenied do |exception|
+      redirect_to root_url, alert: exception.message
+    end
   end
 end
--- app/controllers/admin_panel/dashboard_controller.rb
+++ app/controllers/admin_panel/dashboard_controller.rb
@@ -1,5 +1,7 @@
 module AdminPanel
   class DashboardController < BaseController
+    skip_load_and_authorize_resource
+
     def index; end
   end
 end
